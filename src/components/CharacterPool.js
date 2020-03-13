f34683556d92bc35295a0ed627ca5cc49f8ea36e
faultde <zachary.coxe@gmail.com>
2020-03-13 17:19:38 -0500

    2 files modified + CharacterCard added

--- src/components/CharacterPool.js
+++ src/components/CharacterPool.js
@@ -1,47 +1,8 @@
 import React from "react";
+import CharacterCard from "./CharacterCard";
 
 export default class CharacterPool extends React.Component {
   render() {
-    let alphaArray = this.props.alphaArray;
-
-    const characters = alphaArray.map((character, index) => {
-      return (
-        <div className="col-sm-3">
-          <div className="card" style={{display:'inline-block'}}>
-            <h5 className="card-header">
-              {String.fromCharCode(
-                alphaArray.indexOf(character) + 97
-              ).toUpperCase()}
-            </h5>
-
-            {character.map(element => {
-              return (
-                <button
-                  className="btn btn-sm btn-outline-primary"
-                  onClick={this.props.onCharacterClick}
-                  value={index}
-                  style={{width:50, margin:5, fontSize:"2em"}}
-
-                >
-                  {element}
-                </button>
-              );
-            })}
-
-            <button
-              href="#"
-              className="btn btn-primary"
-              style={{ margin: "1em" }}
-              onClick={this.props.addNew}
-              value={alphaArray.indexOf(character)}
-            >
-              +
-            </button>
-          </div>
-        </div>
-      );
-    });
-
     return (
       <div className="container-lg">
         <div className="card" style={{ marginTop: 20, textAlign: "center" }}>
@@ -67,7 +28,12 @@ export default class CharacterPool extends React.Component {
               Delete
             </button>
           </nav>
-            <div className="row">{characters}</div>
+            <div className="row">
+            <CharacterCard 
+            onCharacterClick={this.props.onCharacterClick}
+            alphaArray={this.props.alphaArray}
+            addNew={this.props.addNew} />
+            </div>
           </div>
         </div>
       </div>
